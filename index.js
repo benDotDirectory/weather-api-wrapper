a587aee5c3478951e4dedbad17cb3483586d4512
ben <bendotbike@gmail.com>
2021-07-20 21:20:56 -0400

    Point routes to API endpoints
    Previously was pointing to a locally hosted express server serving dummy 'endpoints' for testing

--- index.js
+++ index.js
@@ -1,59 +1,45 @@
 /*
-*   weather-dashboard
+*   weather-api-test
 *   index.js
 *
-*   Main entry point. Run: "npm start" or "npm test"
+*   Main entry point. Run "npm start" or "npm test"
 */
 
-/* Imports & config */
+// Imports & config
+const fs = require("fs");
 const path = require("path");
 
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 
-const production = (process.env.PRODUCTION == "true") ? true : false || false;
-
-const https = require("https");
-const axios = require("axios");
-
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 4040;
-app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, "/views")));
+const port = process.env.PORT || 4000;
 
-const routes = require(path.join(__dirname, "/routes.js"));
-app.use("/", routes);
+const pug = require("pug");
+const axios = require("axios");
 
-const logger = require(path.join(__dirname, "/logger.js")).logger;
+// Production?
+let productionEnabled;
+if (process.env.PRODUCTION == "true") {
+    productionEnabled = true;
+} else {
+    productionEnabled = false;
+}
 
-logger.info("** weather-dashboard **");
-logger.info("Logging started: " + new Date());
-
-// Configure API (https://ambientweather.docs.apiary.io/)
-const macAddress = process.env.MAC_ADDRESS || logger.error("No MAC_ADDRESS in .env!");
-const apiKey = process.env.API_KEY || logger.error("No API_KEY in .env!");
-const appKey = process.env.APP_KEY || logger.error("No APP_KEY in .env!");
-
-//const apiBase = "https://private-anon-c466bd994e-ambientweather.apiary-proxy.com/v1/devices/";
-const apiBase = "https://api.ambientweather.net/v1/devices";
-const keys = "?applicationKey=" + appKey + "&apiKey=" + apiKey;
-
-// Get device data
-app.get("/device-data", (req, res) => {
-    const url = apiBase + macAddress + keys + "&limit=5";
-    https.get(url, (resp) => {
-        let data = "";
-        resp.on("data", (chunk) => {
-            data += chunk;
-        });
-
-        resp.on("end", () => {
-            res.send(data);
-        });
-    });
+const winston = require("winston");
+const logFile = process.env.LOG_FILE || path.join(__dirname, "/logs/log.txt");
+const logger = winston.createLogger({
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.File({ filename: logFile })
+    ]
 });
 
-/* Start express server */
+if (!productionEnabled) {
+    logger.add(new winston.transports.Console({ format: winston.format.simple() }));
+}
+
+// Start express
 app.listen(port, () => {
-    logger.info("Express server started. (production: " + production + ", port: " + port + ")");
+    logger.info("Express server listening (port: " + port + ")");
 });
